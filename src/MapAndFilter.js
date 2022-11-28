d18c5a2b0ca396bd965570d55662def4ac90e7d1
Basit Malik <mbasit467@gmail.com>
2022-11-28 20:48:58 +0500

    Update MapAndFilter.js

--- src/MapAndFilter.js
+++ src/MapAndFilter.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import "./MapAndFilter.css";
+
 function MapAndFilter() 
 {
     const [card ,  setCard] = useState([
         {
+            id: "1",
             name: "Basit",
             class: "BSE-4B",
             RollNo: "Sp21-BSE-054",
@@ -11,6 +13,7 @@ function MapAndFilter()
 
         },
         {
+            id: "2",
             name: "Nazeef",
             class: "BSE-4B",
             RollNo: "Sp21-BSE-068",
@@ -18,6 +21,7 @@ function MapAndFilter()
 
         },
         {
+            id: "3",
             name: "Mashood",
             class: "BSE-4B",
             RollNo: "Sp21-BSE-045",
@@ -25,6 +29,7 @@ function MapAndFilter()
 
         },
         {
+            id: "4",
             name: "Huraira",
             class: "BSE-4B",
             RollNo: "Sp21-BSE-043",
@@ -32,6 +37,7 @@ function MapAndFilter()
 
         },
         {
+            id: "5",
             name: "Irfan",
             class: "BSE-4B",
             RollNo: "Sp21-BSE-063",
@@ -39,16 +45,24 @@ function MapAndFilter()
 
         }
     ])
+
+    function handleDelete(id)
+    {
+        const Delete = card.filter((cardDelete)=>cardDelete.id !== id);
+        setCard(Delete);
+
+    }
     return ( 
         <div>
             {
                 card.map((displayCard)=>
                 (
-                    <div className="card">
+                    <div className="card" key={displayCard.id}>
                     <h5>Name: {displayCard.name}</h5>
                     <h5>Class: {displayCard.class}</h5>
                     <h5>Roll Number: {displayCard.RollNo}</h5>
                     <h5>University: {displayCard.university}</h5>
+                    <button className="delete-btn" onClick={()=>handleDelete(displayCard.id)}>Delete</button>
                     </div>
                 ))
             }
